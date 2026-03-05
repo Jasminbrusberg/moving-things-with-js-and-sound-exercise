bdc46adcd09a5e316c3631cdc0eca8b0fa3b28c9
Jasminbrusberg <J-brusberg@hotmail.com>
2026-03-05 21:54:45 +0100

    warm-up

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
 
 
 const dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#000000";
-dodger.style.backgroundColor = "#FF69B4";
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+
+
+
 
