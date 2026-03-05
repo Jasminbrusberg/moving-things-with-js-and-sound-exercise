0037a5198b195a11007bee9bdfbfeccd6e47e261
Jasminbrusberg <J-brusberg@hotmail.com>
2026-03-05 23:00:02 +0100

    exercise-2

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function moveDodgerLeft() {
   }
 }
 
-
 function moveDodgerRight () {
   const leftNumbers = dodger.style.left.replace("px", ""); 
   const left = parseInt(leftNumbers, 10); 
@@ -21,15 +20,24 @@ function moveDodgerRight () {
     if (left < 360 ) {
     dodger.style.left = `${left + 1}px`;
   }
-
-
-  
 }
+
+function moveDodgerUp () {
+const bottomNumbers = dodger.style.bottom.replace("px", "")
+const bottom = parseInt(bottomNumbers, 10); 
+
+if (bottom < 360) {
+    dodger.style.bottom = `${bottom + 1}px`;
+}
+}
+
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") moveDodgerLeft();
   if (e.key === "ArrowRight") moveDodgerRight();
+  if (e.key === "ArrowUp") moveDodgerUp();
 });
+
 
 
 
